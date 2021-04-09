d705e29a173a2bd141ea4e6954abd14f83287fa5
juchilian <juchilian1227@gmail.com>
2021-04-09 11:24:03 +0900

    [update] Mining block(Proof of Work)

--- main.js
+++ main.js
@@ -1,22 +1,33 @@
 const SHA256 = require('crypto-js/sha256')
+
 class Block {
   constructor(index, timestamp, data, previousHash = '') {
-    this.index = index,
-      this.timestamp = timestamp,
-      this.data = data,
-      this.previousHash = previousHash,
-      this.hash = this.calculateHash();
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+    this.nonce = 0;
   }
 
   calculateHash() {
-    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+  }
+
+  mineBlock(difficulty) {
+    while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+
+    console.log("Block mined: " + this.hash);
   }
 }
 
 class BlockChain {
   constructor() {
     this.chain = [this.createGenesisBlock()];
-    
+    this.difficulty = 4;
   }
 
   createGenesisBlock() {
@@ -29,7 +40,7 @@ class BlockChain {
 
   addBlock(newBlock) {
     newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
+    newBlock.mineBlock(this.difficulty);
     this.chain.push(newBlock);
   }
 
@@ -51,14 +62,17 @@ class BlockChain {
 }
 
 let rereCoin = new BlockChain();
+
+console.log("Mining Block 1...");
 rereCoin.addBlock(new Block(1, "2020/04/09", { amount: 4 }));
+console.log("Mining Block 2...");
 rereCoin.addBlock(new Block(2, "2020/04/09", { amount: 12 }));
 
-// console.log(rereCoin);
-console.log('Is blockchain valid? ' + rereCoin.isChainValid());
+// // Tampering
+// rereCoin.chain[1].data = { amount: 100 };
+// rereCoin.chain[1].hash = rereCoin.chain[1].calculateHash();
 
-rereCoin.chain[1].data = { amount: 100 };
-rereCoin.chain[1].hash = rereCoin.chain[1].calculateHash();
-
+// // Check blockchain
 // console.log(rereCoin);
-console.log('Is blockchain valid? ' + rereCoin.isChainValid());
+// console.log('Is blockchain valid? ' + rereCoin.isChainValid());
+

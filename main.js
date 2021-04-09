c906afa8af38646e2c3401caeddce217064108d9
juchilian <juchilian1227@gmail.com>
2021-04-09 11:12:48 +0900

    [update] class Blockchain

--- main.js
+++ main.js
@@ -13,3 +13,52 @@ class Block {
   }
 }
 
+class BlockChain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+    
+  }
+
+  createGenesisBlock() {
+    return new Block(0, "2020/04/09", "Genesis block", "0")
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const prevBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== prevBlock.hash) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+let rereCoin = new BlockChain();
+rereCoin.addBlock(new Block(1, "2020/04/09", { amount: 4 }));
+rereCoin.addBlock(new Block(2, "2020/04/09", { amount: 12 }));
+
+// console.log(rereCoin);
+console.log('Is blockchain valid? ' + rereCoin.isChainValid());
+
+rereCoin.chain[1].data = { amount: 100 };
+rereCoin.chain[1].hash = rereCoin.chain[1].calculateHash();
+
+// console.log(rereCoin);
+console.log('Is blockchain valid? ' + rereCoin.isChainValid());
